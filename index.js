62c8d79f951cd8e0c7e87e6264650134d8dc6bc7
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-10-19 19:57:52 +0200

    .gsap.to more items

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-const frame = document.querySelector('.frame');
+const frame = document.querySelectorAll('.frame');
 
 gsap.set(frame, { visibility: 'visible' });
 
-// item / time / start:animation / stop:animation
-gsap.fromTo(
-	frame,
-	5,
-	{
-		opacity: 0,
-		backgroundColor: 'pink'
-	},
-	{
-		opacity: 0.5,
-		backgroundColor: 'blue'
-	}
-);
-
-gsap.to(frame, 2, { x: 300, delay: 2.5 });
+gsap.to(frame, { duration: 2,
+    stagger: .5, opacity: 0, x: 200,});

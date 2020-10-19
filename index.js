e942dd70b66612229c013efeba11f6f6d1d33aea
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-10-19 20:09:12 +0200

    timeline

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const frame = document.querySelectorAll('.frame');
 
-gsap.set(frame, { visibility: 'visible' });
+let tl = gsap.timeline({delay: 2});
 
-gsap.to(frame, { duration: 2,
-    stagger: .5, opacity: 0, x: 200,});
+tl.set(frame, { visibility: 'visible' });
+
+tl.to(frame[0], { duration: 1, x: -50, y: 50 }).to(
+	frame[2],
+	{
+		duration: 1,
+		x: 50,
+		y: -50
+	},
+	'-=1'
+);

f06bb2f6570dc62c881b86b23593287cbf1b1df2
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-10-19 20:36:01 +0200

    More timeline efects, addLabel

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let tl = gsap.timeline({ delay: 2 });
 tl.set(frames, { visibility: 'visible' });
 
 tl.from(frames, { duration: 2, opacity: 0 })
+	.addLabel('moveBoxes')
 	.to(frames[0], { duration: 1, x: -50, y: 50 })
 	.to(
 		frames[2],
@@ -13,5 +14,33 @@ tl.from(frames, { duration: 2, opacity: 0 })
 			x: 50,
 			y: -50
 		},
-		'+=5'
-	);
+		'moveBoxes'
+	)
+	.addLabel('boxesOnSides')
+	.to([frames[0], frames[2]], {
+		duration: 2,
+		backgroundColor: 'hsl(0, 0%, 5%)',
+		borderColor: 'transparent'
+	})
+	.to(
+		frames[0],
+		{
+			duration: 1,
+			x: -300,
+			y: 0
+		},
+		'boxesOnSides'
+	)
+	.to(
+		frames[2],
+		{
+			duration: 1,
+			x: 300,
+			y: 0
+		},
+		'boxesOnSides'
+	)
+	.addLabel('hideBehindBoxes')
+	.set([frames[0], frames[2]], { zIndex: 99 })
+	.to(frames[1], { duration: 2, backgroundColor: 'white' })
+	.to([frames[0], frames[2]], { duration: 5, x: 0 }, 'hideBehindBoxes+=3');
